1ac376fec8b9089157dfee32e0808fe319a5c6da
MinhHoang <minhhoangdayyyy07@gmail.com>
2026-06-22 21:14:37 +0700

    update for railway

--- src/main/java/com/example/demo/entity/Message.java
+++ src/main/java/com/example/demo/entity/Message.java
@@ -1,11 +1,27 @@
 package com.example.demo.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
-import lombok.*;
+import java.time.LocalDateTime;
+
 import org.hibernate.annotations.CreationTimestamp;
 
-import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "Messages")
@@ -30,7 +46,8 @@ public class Message {
     private String content;
     
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false, columnDefinition = "ENUM('text', 'image', 'video', 'file', 'audio') DEFAULT 'text'")
+    @Column(nullable = false, length = 20)
+    //    @Column(nullable = false, columnDefinition = "ENUM('text', 'image', 'video', 'file', 'audio') DEFAULT 'text'")
     private MessageType messageType = MessageType.TEXT;
     
     @Column(length = 1000)
